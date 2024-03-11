95ab5228d0f46463e862d96c28562a7cca9c701a
SametAltintas <samet236236@gmail.com>
2024-03-11 19:23:16 +0300

    latest-changes

---
+++ intro.js
@@ -0,0 +1,39 @@
+// console.log("senkron");
+// // alert("alert")
+// console.log("salamlar");
+
+// const gecikme = (sure) =>{
+//     const basla = new Date().getTime()
+//     while(new Date().getTime() < basla + sure){
+
+//     }
+// }
+// // gecikme(3000)
+// // console.log("bitti");
+// console.log("kuruldu");
+// const timeoutId = setTimeout(()=>{
+//     console.log("sure doldu");
+// },2000)
+// console.log("bitti");
+// // console.time("basla")
+// // gecikme(2000)
+// // console.timeEnd("basla")
+// let i=0
+// const zaman = setInterval(()=>{
+//     console.log(++i);
+//     if (i>=5) {
+//         clearInterval(zaman)
+//     }
+// },1000)
+setTimeout(()=>{
+    console.log("sa");
+    setTimeout(()=>{
+        console.log("as");
+        setTimeout(() => {
+            console.log("nbr");
+            setTimeout(() => {
+                console.log("iyidir");
+            }, 1000);
+        }, 1000);
+    },1000)
+},1000)
