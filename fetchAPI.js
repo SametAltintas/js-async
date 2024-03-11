6595ff6767ef365ded26d0c4315b5412e86ba0ce
SametAltintas <samet236236@gmail.com>
2024-03-11 23:36:13 +0300

    json-printed

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,1 +1,10 @@
 console.log("FETCH");
+// fetch("https://api.github.com/users").then((res)=>{
+//     console.log(res)
+//     console.log(res.ok)
+//     console.log(res.status)
+//     return res
+// }).then((res)=>res.json())
+fetch("https://api.github.com/users")
+    .then((res)=>res.json())
+    .then((data)=>console.log(data))
