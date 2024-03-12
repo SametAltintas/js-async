fe218c4ac90a297b67bce01eadd5277ab80088be
SametAltintas <samet236236@gmail.com>
2024-03-12 21:38:11 +0300

    project-has-been-finished

--- async1/fetchAPI.js
+++ async1/fetchAPI.js
@@ -1,11 +1,38 @@
 console.log("FETCH");
 // fetch("https://api.github.com/users").then((res)=>{
 //     console.log(res)
+//     if (!res.ok) {
+//         throw new Error(`Hata: ${res.status}`)
+//     }
 //     console.log(res.ok)
 //     console.log(res.status)
 //     return res
 // }).then((res)=>res.json())
+// fetch("https://api.github.com/users")
+//     .then((res)=>res.json())
+//     .then((data)=>console.log(data))
+//     .catch((err)=> console.log(err))
+let veri = "";
 fetch("https://api.github.com/users")
-    .then((res)=>res.json())
-    .then((data)=>console.log(data))
-    
+    .then((res) => {
+        console.log(res);
+        if (!res.ok) {
+            throw new Error(`Hata: ${res.status}`);
+        }
+        return res.json();
+    })
+    .then((data) => {
+        veri = data;
+        show(veri);
+    })
+    .catch((err) => document.write(err));
+
+const show = (users) => {
+    const userSection = document.getElementById("users");
+    users.forEach((user) => {
+        userSection.innerHTML += `
+            <h1>${user.login}</h1>
+            <img src="${user.avatar_url}" alt=""/>
+        `;
+    });
+};
